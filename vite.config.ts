210d548ebd433dffa4b7d263f6a37f4d3f0e37ba
aaakash06 <akashbagale182@gmail.com>
2025-05-07 02:05:16 +0545

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": resolve(__dirname, "./src"),
-      components: resolve(__dirname, "./src/components"),
-      features: resolve(__dirname, "./src/features"),
-      state: resolve(__dirname, "./src/state"),
-    },
-  },
 });
